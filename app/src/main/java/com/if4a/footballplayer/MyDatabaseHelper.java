b977445324a9b7b3aa50cdf8a05ea9f5e2cc1933
Handy Christianto Tan_2125250049 <113890291+HandyChristiantoTan@users.noreply.github.com>
2023-03-08 09:33:32 +0700

    Update MyDatabaseHelper.java

--- app/src/main/java/com/if4a/footballplayer/MyDatabaseHelper.java
+++ app/src/main/java/com/if4a/footballplayer/MyDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.if4a.footballplayer;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -24,17 +25,38 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE" + TABLE_NAME + "( " +
+        String query = "CREATE TABLE " + TABLE_NAME + "( " +
                 FIELD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 FIELD_NAMA + " VARCHAR(50), " +
                 FIELD_NOMOR + " VARCHAR(2), " +
+<<<<<<< HEAD
+                FIELD_KLUB + " VARCHAR(50) " +
+=======
                 FIELD_KLUB + " VARCHAR(50), " +
+<<<<<<< HEAD
+>>>>>>> 017de022074710b01ffff2fa7b7e76ed7d96d811
+=======
+>>>>>>> 017de022074710b01ffff2fa7b7e76ed7d96d811
                 ");";
         db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
 
+    public long tambahPlayer(String nama, String nomor, String klub){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(FIELD_NAMA, nama);
+        cv.put(FIELD_NOMOR, nomor);
+        cv.put(FIELD_KLUB, klub);
+
+        long eksekusi = db.insert(TABLE_NAME, null, cv);
+        return eksekusi;
     }
 }
